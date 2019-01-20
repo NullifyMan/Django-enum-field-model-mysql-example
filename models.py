90145d6464973fdf645a22f4496fd2bc652e6d42
lipovich <46872634+lipovich@users.noreply.github.com>
2019-01-20 22:30:41 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class EField(models.Field):
+	def __init__(self, val, *args, **kwargs):
+		# self.vals = **kwargs
+		self.val = val
+
+		super().__init__(*args, **kwargs)
+		# kwargs['val'] = self.val
+
+	def db_type(self, connection):
+		# val = "ENUM(#)"
+		# val.replace("#", self.vals)
+		return 'ENUM(%s)' % self.val
+
+	def deconstruct(self):
+		name, path, args, kwargs = super().deconstruct()
+		kwargs['val'] = self.val
+		return name, path, args, kwargs
+    
+class receiveCalls(models.Model):
+	test_bet = EField(val = "hi,wor", default='hi')
