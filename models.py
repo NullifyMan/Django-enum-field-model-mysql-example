f2fd0146326850687b2ef0403473f8ee53f4327c
lipovich <46872634+lipovich@users.noreply.github.com>
2019-01-20 22:35:13 +0300

    Update models.py

--- models.py
+++ models.py
@@ -3,9 +3,7 @@ from django.db import models
 class EField(models.Field):
 	def __init__(self, val, *args, **kwargs):
 		self.val = val
-
 		super().__init__(*args, **kwargs)
-		# kwargs['val'] = self.val
 
 	def db_type(self, connection):
 		return 'ENUM(%s)' % self.val
